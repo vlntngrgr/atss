0278b31a9041879c9f38aa9cf36515f6dc90bffd
Valentin Grégoire <valentin@infinity-mobile.io>
2019-12-15 14:03:49 +0100

    reset project

--- README.md
+++
@@ -1,8 +0,0 @@
-# Arnaud Toussain Services Serrurie
-
-
-
-
-
-
-> this project was bootstrapped with <http://github.com/sw-yx/create-react-app-parcel>
--- src/client/components/galerie/ItemGalerie.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ItemGalerie = () => {
-    return <article>{"Item Galerie"}</article>;
-};
-
-export default ItemGalerie;
